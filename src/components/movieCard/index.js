73c56f0df1c9713b855175bdcc3c39efb8b2802c
Trysten Afrin <trystenafrin10@gmail.com>
2020-11-03 21:01:28 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -4,7 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../globals/fontawesome";
 import { Link } from "react-router-dom";
 
-const MovieCard = props => {
+const MovieCard = (props) => {
+
+  const handleAddToFavorites = e => {
+    e.preventDefault()
+    props.buttonHandler(props.movie.id)  // new line
+  }
+
   return (
     <div className="col-sm-3">
       <div className="card  bg-white">
@@ -19,7 +25,6 @@ const MovieCard = props => {
             }
           />
         </Link>
-
         <div className="card-body">
           <h4 className="card-title ">{props.movie.title}</h4>
           <p>
@@ -32,7 +37,9 @@ const MovieCard = props => {
           </p>
         </div>
         <div className="card-footer">
-          <button type="button" className="btn w-100 btn-primary">
+          <button type="button" className="btn w-100 btn-primary"
+                onClick={handleAddToFavorites}
+                >
             Add to Favorites
           </button>
         </div>
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
+import StubAPI from "../api/stubAPI";
 
 const MovieListPage = () => {
   const [titleFilter, setTitleFilter] = useState("");       // NEW
@@ -22,7 +23,7 @@ const MovieListPage = () => {
         setMovies(movies);
       });
   })
-  // NEW BLOCK START
+  
   const genre = Number(genreFilter)
   let displayedMovies = movies
     .filter(m => {
@@ -36,12 +37,25 @@ const MovieListPage = () => {
     if (type === "name") setTitleFilter(value);
     else setGenreFilter(value);
   };
-  // NEW BLOCK END
+
+  const addToFavorites = movieId => {
+    // Find index position of selected movie in the list
+    const index = movies.map(m => m.id).indexOf(movieId)
+
+    StubAPI.add(movies[index])
+    const updatedList = [...movies]  // Make a copy of the movie list
+    updatedList.splice(index, 1)     // Remove selected movie from home page list
+    setMovies(updatedList)  
+  }
+  
   return (
     <>
-      <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
-      <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
-      <MovieList movies={displayedMovies} />                  {/* CHANGED */}
+      <Header numMovies={displayedMovies.length} />
+      <FilterControls onUserInput={handleFilterChange} />
+      <MovieList
+        movies={displayedMovies}
+        buttonHandler={addToFavorites}
+      />     
     </>
   );
 };
